b8fe4393010c7f7df86aa9476281cb6d53d0849f
carlopra23 <pradojohncarlo124@gmail.com>
2024-05-25 18:13:26 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,7 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
 
+function clickLike(){
+  let totalLikes = parseInt(countlLike1.value) + 1
+  countLike1.textContent = totalLikes.toString()
+}
