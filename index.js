7994ad790615247d7ebd3979f93e899489ffd50a
carlopra23 <pradojohncarlo124@gmail.com>
2024-06-08 13:22:16 +0800

    Update and rename Index.js to index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const btnLike1 = document.getElementById("btnLike1")
 const countLike1 = document.getElementById("countLike1")
 
-function clickLike(){
+function clickLike1(){
   let totalLikes = parseInt(countlLike1.value) + 1
   countLike1.textContent = totalLikes.toString()
 }
 btnLike1.AddEventListener("click",clickLike1)
+
+function clickLike2(){
+  let totalLikes = parseInt(countlLike2.value) + 1
+  countLike2.textContent = totalLikes.toString()
+}
+btnLike2.AddEventListener("click",clickLike2)
