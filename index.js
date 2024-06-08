4eabf5c99d207997baea0325be8d67d392585799
carlopra23 <pradojohncarlo124@gmail.com>
2024-06-08 18:18:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ function clickLike1(){
 }
 btnLike1.addEventListener("click",clickLike1)
 
-const btnLike2 = document.getElementById("btnLike2")
-const countLike2 = document.getElementById("countLike2")
-
 function clickLike2(){
   let totalLikes = parseInt(countLike2.value) + 1
   countLike1.textContent = totalLikes.toString()
@@ -17,14 +14,10 @@ function clickLike2(){
 btnLike2.addEventListener("click",clickLike2)
 
 const submit = document.getElementById("submit")
-
 const comment = document.getElementById("comment")
-
 const commentbox = document.getElementById("commentbox")
-
 function submitComment(){
-
-commentbox.textContent += comment.value.toString() + "\n"
+  commentbox.textContent += comment.value.toString() + "\n"
 
 }
 
